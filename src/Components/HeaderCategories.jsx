0fc5789ce147d1aac21dc236ef80933c7db7ef24
ivanosquis10 <83567373+ivanosquis10@users.noreply.github.com>
2023-02-11 14:44:43 -0800

    se arreglo el titulo de las categorias

--- src/Components/HeaderCategories.jsx
+++ src/Components/HeaderCategories.jsx
@@ -1,6 +1,6 @@
 const HeaderCategories = ({ heading }) => {
   return (
-    <h3 className="bg-slate-700 w-fit mx-auto text-slate-300 font-medium text-center text-base uppercase md:text-2xl px-5 py-2 rounded-md shadow-md shadow-gray-500 mb-10">
+    <h3 className="md:w-10/12 mx-auto text-indigo-500 font-bold text-center text-base uppercase md:text-2xl px-5 py-2 rounded-md shadow shadow-indigo-700 mb-10">
       {heading}
     </h3>
   );
